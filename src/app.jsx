85aa21748608ea05f89a94c2429512a9b0a25c0b
Rafiur Rahman <rrfoundary@gmail.com>
2022-05-01 17:46:33 +0600

    added sass and react router dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import "./app.scss";
+import Auth from "./routes/auth/auth.component";
 const App = () => {
-  return <div>app</div>;
+  return (
+    <Routes>
+      <Route path="/" element={Auth} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/auth/auth.component.jsx
@@ -0,0 +1,6 @@
+import "./auth.style.scss";
+const Auth = () => {
+  return <div className="auth">Authentication</div>;
+};
+
+export default Auth;
