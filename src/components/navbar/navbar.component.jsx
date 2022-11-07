4266a377c159033cfbe6e4e8aab32a96078bfb0e
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-07 20:08:27 +0600

    added navbar link with styles

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,14 +1,27 @@
 import "./navbar.style.scss";
+import { FaHome, FaSearch } from "react-icons/fa";
+import { BiLibrary } from "react-icons/bi";
+import { BsSuitHeartFill, BsFillPlusCircleFill } from "react-icons/bs";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Logo</h1>
+      <h1 className="mb-xl">Logo</h1>
       <div className="navbar-menu">
-        <a href="">Home</a>
-        <a href="">Search</a>
-        <a href="">Your Library</a>
-        <a href="">Create Playlist</a>
-        <a href="">Liked songs</a>
+        <a href="" className="mb-m">
+          <FaHome /> Home
+        </a>
+        <a href="" className="mb-m">
+          <FaSearch /> Search
+        </a>
+        <a href="" className="mb-xl">
+          <BiLibrary /> Your Library
+        </a>
+        <a href="" className="mb-m">
+          <BsFillPlusCircleFill /> Create Playlist
+        </a>
+        <a href="" className="mb-m">
+          <BsSuitHeartFill /> Liked songs
+        </a>
       </div>
       <div className="navbar-playlist">
         <a href="">Playlist #1</a>
