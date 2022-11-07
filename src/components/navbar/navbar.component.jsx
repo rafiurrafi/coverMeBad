105356930ecddc6cff9778f5ebec20672d470f00
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-07 18:47:21 +0600

    added fonts and basic styles

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,7 +2,17 @@ import "./navbar.style.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Navbar</h1>
+      <h1>Logo</h1>
+      <div className="navbar-menu">
+        <a href="">Home</a>
+        <a href="">Search</a>
+        <a href="">Your Library</a>
+        <a href="">Create Playlist</a>
+        <a href="">Liked songs</a>
+      </div>
+      <div className="navbar-playlist">
+        <a href="">Playlist #1</a>
+      </div>
     </div>
   );
 };
