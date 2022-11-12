d504dab228fe2d1d71a3204a97e9deb40642c310
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-12 19:48:44 +0600

    added playlist in genre

---
+++ src/fake-server/fakePlaylistService.js
@@ -0,0 +1,14 @@
+const PlaylistService = [
+  {
+    _id: "playlist-",
+    title: "Nehu",
+    cover: "assets/albums/album-2.jpeg",
+    songs: ["song-1"],
+  },
+  {
+    _id: "playlist-",
+    title: "Street -2",
+    cover: "assets/albums/album-3.jpeg",
+    songs: ["song-2"],
+  },
+];
